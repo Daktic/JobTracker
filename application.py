a0ed94af66b90ca988b9c3579ad4ba217c58e675
Daktic <g@eye-catcher.com>
2024-01-14 17:24:18 -0500

    Created first steps in the job tracker. outlined some things I would like to do and made some basic classes for it.

---
+++ application.py
@@ -0,0 +1,76 @@
+import datetime
+import pandas as pd
+from enum import Enum
+
+
+class JobList:
+    """
+    This class should hold the data responsible for translating job details into the Google Sheet
+    From here you can do crud operations like adding, deleting, and updating a record.
+    """
+
+    def __init__(self):
+        self.jobs = []
+
+    # Add a job to the job tracker
+    def add_job(self, company: str, title: str, locations: [str], site: str):
+        new_job = Job(company, title, locations, site)
+        self.jobs.append(new_job)
+
+    # Convert the joblist into a dataframe, this makes it easier to upload to google
+    def to_df(self):
+        df = pd.DataFrame()
+
+        for job in self.jobs:
+            job_line = [
+                job.application.application_date,
+                job.company,
+                job.title,
+                job.locations.join(", "),
+                job.site,
+                job.application.response_date,
+                job.application.status_code
+            ]
+            df = df.append(job_line, ignore_index=True)
+
+
+# For updating status on the application
+class Status(Enum):
+    Pending = "Pending"
+    Accepted = "Accepted"
+    Rejected = "Rejected"
+
+
+class Job:
+    """
+    The Job identifies characteristics about the job itself. It also creates new applications from the job.
+    """
+
+    def __init__(self, company: str, title: str, locations: [str], site: str) -> None:
+        self.company = company
+        self.title = title
+        self.locations = locations
+        self.site = site
+        self.application = self.create_application()
+
+    def create_application(self):
+        return Application()
+
+    def record_response(self, status: Status):
+        self.application.update_status(status)
+
+
+class Application:
+    """
+    The Application creates a timestamp of its creation and sets the application status as pending.
+    It can be modified to reflect new information in status and response date.
+    """
+
+    def __init__(self):
+        self.application_date = datetime.datetime.now()
+        self.status = Status.Pending
+        self.response_date = None
+
+    def update_status(self, status: Status):
+        self.status = status
+        self.response_date = datetime.datetime.now()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Hello
+## This is a fun little tool to help keep track of job applications.
+### It may grow with time if I feel like adding to it, but it mainly just adds jobs to a Google sheet.
