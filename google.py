4a89040be526b81e7de5d55144ed35a71390e1d9
Daktic <gberg@DakticDigital.com>
2024-01-14 21:50:35 -0500

    Working on getting the service credentials from the expected file

--- google.py
+++ google.py
@@ -1,4 +1,22 @@
+import json
+import os
+import re
 
 
 class Credentials:
-    d
+    def __init__(self):
+        self.client_id = None
+
+    def get_credentials(self):
+        """
+        This function will attempt to find the crednetials.json file in the current directory. If it is not found, it will exit the program.
+        """
+        try:
+            current_dir = os.getcwd()
+            for filename in os.listdir(current_dir+"/google_credentials"):
+                if re.match(".*/.json", filename):
+                    cred_json = json.load(open(filename, "r"))
+                    print(cred_json)
+        except FileNotFoundError:
+            print("credentials.json not found. Please place the file in the current directory.")
+            exit(1)
