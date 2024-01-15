c5af4dae6c7f7e1a01040b7ed37e671a58a5d181
Daktic <gberg@DakticDigital.com>
2024-01-15 00:01:36 -0500

    Updated the test to create a fake credential file, if it does not exist. then test if the client id is the expected on in either case.

--- test_module.py
+++ test_module.py
@@ -1,27 +1,35 @@
+import json
 import os
 import unittest
 from google import Credentials
 
+
 class MyTestCase(unittest.TestCase):
     def test_finds_credentials(self):
-        # Add fake credentials.json file
-        fake_credentials_path = os.path.join(os.getcwd(), "google_credentials", "a.json")
 
-        with open(fake_credentials_path, "w") as f:
-            f.write('{"client_id": "123456789"}')
+        credential_path = os.path.join(os.getcwd(), "google_credentials")
 
-        # Get the list of files and ensure fake credentials file is the first
-        files = os.listdir(os.path.join(os.getcwd(), "google_credentials"))
-        files.sort()
-        files.insert(0, "a.json")
+        # Check if the credential file exists
+        if len(os.listdir(credential_path)) == 0:
+            with open(credential_path + "/credentials.json", "w") as f:  # Create a fake credentials file
+                f.write('{"client_id": "123456789"}')
+        elif len(os.listdir(credential_path)) > 1:  # Check if there are too many files in the directory
+            print("Too many files in google_credentials directory. Please remove all but one file.")
+            exit(1)
 
         # Instantiate Credentials class and test
         creds = Credentials()
         creds.get_credentials()
         try:
-            self.assertEqual('123456789', creds.client_id)  # Test if the client_id is correct
+            filename = os.listdir(credential_path)[0]  # Assumes only one file in the directory
+            f = open(os.path.join(credential_path, filename), "r")
+            cred_json = json.load(f)
+            client_id = cred_json["client_id"]
+            self.assertEqual(client_id, creds.client_id)
+            f.close()
         finally:
-            os.remove(fake_credentials_path)
+            if os.path.exists(credential_path + "/credentials.json"):
+                os.remove(credential_path + "/credentials.json")
 
 
 if __name__ == '__main__':
