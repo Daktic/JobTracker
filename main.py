480f8819144521514e9b7f9589339bf1daa61c98
Daktic <gberg@DakticDigital.com>
2024-01-15 23:46:02 -0500

    Playing around with the GUI

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
+import json
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Google Sheet Manager")
+def get_config():
+    with open("config.json", "r") as f:
+        return json.load(f)
 
-root.mainloop()
+
+def app():
+    config = get_config()
+
+    root = tk.Tk()
+    root.title("Google Sheet Manager")
+
+
+    root.geometry("500x500")
+    # Set background color to white
+    root.configure(bg="white")
+
+
+    label = tk.Label(root, text="Hello, Tkinter!")
+    label.pack()  # Use pack() to add it to the window
+    root.mainloop()
+
+if __name__ == "__main__":
+    app()
