2cf566ed79d9d7fab9a53dcdfc6ba6e69e03953d
Daktic <g@eye-catcher.com>
2024-01-17 18:02:44 -0500

    Added response time calculations

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 from application import JobList
 from google_interface import Google
+import pandas as pd
+
 
 def get_config():
     with open("config.json", "r") as f:
@@ -9,21 +11,33 @@ def get_config():
 
 class App():
     def __init__(self):
+        self.job_df = None
         self.joblist = JobList()
         self.config = get_config()
         self.google_interface = Google(self.config['sheet_id'], self.config['sheet_name'])
 
     def calculate_metrics(self):
-        g = self.google_interface.get_sheet_df()
-        print(g)
-
+        self.job_df = self.google_interface.get_sheet_df()
+        self.calculate_response_times()
 
     def calculate_response_times(self):
-        pass
-
-
-
-
+        """
+        This will look at the response time in days column and run some stats on it
+        """
+        # Get the columns
+        response_time_col = self.job_df["Response Time(DAYS)"]
+        # Convert to numeric
+        response_time_col.replace("No Response Yet", '', inplace=True)
+        response_time_col = response_time_col.apply(pd.to_numeric)
+        # Calculate response time metrics
+        total_apps = len(response_time_col)
+        num_responses = sum(response_time_col.notnull())
+        print(f"Of the {total_apps} jobs, {num_responses} have responded. "
+              f"{round(total_apps / num_responses, 2)}%")
+        # print out the data types of each value
+        print(f"\nOf those {num_responses}:")
+        print(f"Average response time: {round(response_time_col.mean(), 1)} Days")
+        print(f"Median response time: {response_time_col.median()} Days")
 
 
 if __name__ == "__main__":
