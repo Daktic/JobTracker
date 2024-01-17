8f0d75ca937250fa6c6cb92e4088cb1c9d414764
Daktic <g@eye-catcher.com>
2024-01-17 17:26:25 -0500

    updated google dependecies to use an outer funtion to call the inner functions

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 import json
-import tkinter as tk
+from application import JobList
+from google_interface import Google
 
 def get_config():
     with open("config.json", "r") as f:
         return json.load(f)
 
 
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        tk.Frame.__init__(self, master)
-        self.grid()
-        self.createWidgets()
-        self.master.geometry("500x500")
+class App():
+    def __init__(self):
+        self.joblist = JobList()
+        self.config = get_config()
+        self.google_interface = Google(self.config['sheet_id'], self.config['sheet_name'])
 
+    def do_smth(self):
+        g = self.google_interface.get_sheet_df()
+        jl = self.joblist.from_df(g)
+        print(g)
+        print(jl)
 
-
-    def createWidgets(self):
-        self.quitButton = tk.Button(self, text='Quit',
-            command=self.quit)
-        self.quitButton.grid()
 
 
 
 if __name__ == "__main__":
-    app = Application()
-    app.master.title('Job Tracker')
-    app.mainloop()
+    app = App()
+    app.do_smth()
