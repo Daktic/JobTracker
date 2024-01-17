739511497f3e921b69b89d99aa17eadb4a6e1acd
Daktic <g@eye-catcher.com>
2024-01-17 18:08:14 -0500

    Added company stats

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ class App():
         self.job_df = self.google_interface.get_sheet_df()
         self.calculate_response_times()
         self.calculate_job_titles()
+        self.calculate_company_stats()
 
     def calculate_response_times(self):
         """
@@ -49,7 +50,18 @@ class App():
         # Calculate job type metrics
         distinct_counts = job_type_col.value_counts()
         print(f"\nThere are {len(distinct_counts)} distinct job Titles")
-        print(f"The top 5 job titles are:{distinct_counts.head(5)}")
+        print(f"Top 5 :{distinct_counts.head(5)}")
+
+    def calculate_company_stats(self):
+        """
+        This will look at the company column and run some stats on it
+        """
+        # Get the columns
+        company_col = self.job_df["Company"]
+        # Calculate company metrics
+        distinct_counts = company_col.value_counts()
+        print(f"\nThere are {len(distinct_counts)} distinct companies")
+        print(f"Top 5 :{distinct_counts.head(5)}")
 
 
 if __name__ == "__main__":
